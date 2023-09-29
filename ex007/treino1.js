745195e030309d43e7a5a831b72537660b642e11
Maiko Costa <122563962+Komai-dev@users.noreply.github.com>
2023-09-28 23:08:44 -0300

    fds

---
+++ ex007/treino1.js
@@ -0,0 +1,4 @@
+var nome = "Maiko"
+var idade = 17
+
+console.log(`Olá, meu nome é ${nome} e eu tenho ${idade} anos de idade.`)
---
+++ ex007/treino2.js
@@ -0,0 +1,6 @@
+var n1 = document.getElementById(n1)
+var n2 = document.getElementById(n2)
+function somar() {
+  return n1+n2
+}
+res.appendChild()
